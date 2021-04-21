7f060b8b69d861a978d51a348457e8932392be7b
Britta Kotov <brittakotov@gmail.com>
2021-04-21 09:49:12 -0700

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  var rev = '';
+  var absoluteN = Math.abs(n).toString();
+
+  for (var i = absoluteN.length - 1; i >= 0; i--) {
+    rev += absoluteN[i];
+  }
+
+  while (rev[0] === 0) {
+    rev = rev.slice(1);
+  }
+
+  rev = Number(rev) * Math.sign(n);
+  
+  return rev;
+}
 
 module.exports = reverseInt;
