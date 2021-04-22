6e9ca3f77f91e7b36aa8c22d3e118f57707f9256
Britta Kotov <brittakotov@gmail.com>
2021-04-22 08:27:40 -0700

    fizzbuzz

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,32 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var storage = {};
+  for (var i = 0; i < str.length; i++) {
+    storage[str[i]] = stroage[str[i]] ++ || 1;
+  }
+}
+
+
+
+// function maxChar(str) {
+//   var storage = {};
+
+//   var max = [str[0], 1]
+
+//   for (var i = 0; i < str.length; i++) {
+//     if (storage[str[i]] === undefined) {
+//       storage[str[i]] = 1;
+//     } else {
+//       storage[str[i]]++;
+//     }
+//     if (storage[str[i]] > max[1]) {
+//       max[0] = str[i];
+//       max[1] = storage[str[i]];
+//     }
+//   }
+//   return max[0];
+// }
 
 module.exports = maxChar;
