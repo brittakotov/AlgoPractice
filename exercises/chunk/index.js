3b76db6805f684b381c82542ea5bf99bdc916801
Britta Kotov <brittakotov@gmail.com>
2021-04-26 16:34:52 -0700

    ArrayChunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,23 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//create empty array to hold chunks
+//for each element, retrieve last chunked element
+  //if does not exist, or if length equals chunk size
+    //push a new chunk into chunked
+  //else, add current element into chunk
+  function chunk(array, size) {
+    var chunked = [];
+    for (var i = 0; i < array.length; i++) {
+      var lastChunk = chunked[chunked.length - 1];
+      if (!lastChunk || lastChunk.length === size) {
+        chunked.push([array[i]]);
+      } else {
+        lastChunk.push(array[i]);
+      }
+    }
+    return chunked;
+  }
 
-module.exports = chunk;
+
+  module.exports = chunk;
